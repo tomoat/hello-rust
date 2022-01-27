482bfec02d826f20db7541a8aa0aeb39562b0ecf
tomato <ljc6qx@163.com>
2022-01-27 17:49:17 +0800

    Add multi-language hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    greet_world()
+}
+
+fn greet_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "World, Hello";
+    let regions = [southern_germany, chinese, english];
+
+    for regions in regions.iter() {
+        println!("{}", regions)
+        // println!("{}", &regions)
+    }
 }
