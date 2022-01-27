b361ae17d3b09d894d0057f724e0229edd41b48a
tomato <ljc6qx@163.com>
2022-01-27 18:19:47 +0800

    Add summary comment

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() {
 }
 
 fn greet_world() {
-    let southern_germany = "Grüß Gott!";
-    let chinese = "世界，你好";
-    let english = "World, Hello";
+    let southern_germany = "Grüß Gott! 😊";
+    let chinese = "世界，你好 😊";
+    let english = "World, Hello 😊";
     let regions = [southern_germany, chinese, english];
 
     for regions in regions.iter() {
@@ -14,3 +14,9 @@ fn greet_world() {
         // println!("{}", &regions)
     }
 }
+
+ // Rust 原生支持 UTF-8 编码的字符串
+ // println 后面的 !, 在 Rust 中，这是 宏 操作符，你目前可以认为宏是一种特殊类型函数
+ // println 没有使用 %s,%d 来做输出占位符，而是使用 {},因为 Rust 会自动识别输出数据的类型
+ // Rust 的集合类型不能直接进行循环，需要变成迭代器(这里是通过 .iter() 方法)，才能用于迭代循环
+ // 在 2021 edition 及以后, 支持直接写 for region in regions，原因是因为 for 隐式地将 regions 转换成迭代器
