0eb5fc9d52ce6a3617f450aebec9183e1513e4fa
tomato <ljc6qx@163.com>
2022-02-08 13:18:16 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,11 @@ fn greet_world() {
     }
 }
 
- // Rust 原生支持 UTF-8 编码的字符串
- // println 后面的 !, 在 Rust 中，这是 宏 操作符，你目前可以认为宏是一种特殊类型函数
- // println 没有使用 %s,%d 来做输出占位符，而是使用 {},因为 Rust 会自动识别输出数据的类型
- // Rust 的集合类型不能直接进行循环，需要变成迭代器(这里是通过 .iter() 方法)，才能用于迭代循环
- // 在 2021 edition 及以后, 支持直接写 for region in regions，原因是因为 for 隐式地将 regions 转换成迭代器
-
+// Rust 原生支持 UTF-8 编码的字符串
+// println 后面的 !, 在 Rust 中，这是 宏 操作符，你目前可以认为宏是一种特殊类型函数
+// println 没有使用 %s,%d 来做输出占位符，而是使用 {},因为 Rust 会自动识别输出数据的类型
+// Rust 的集合类型不能直接进行循环，需要变成迭代器(这里是通过 .iter() 方法)，才能用于迭代循环
+// 在 2021 edition 及以后, 支持直接写 for region in regions，原因是因为 for 隐式地将 regions 转换成迭代器
 
 fn penguin() {
     let penguin_data = "\
@@ -46,28 +45,24 @@ fn penguin() {
         }
 
         // 声明一个fields变量，类型是Vec
-     // Vec是vector的缩写，是一个可伸缩的集合类型，可以认为是一个动态数组
-     // <_>表示Vec中的元素类型由编译器自行推断，在很多场景下，都会帮我们省却不少功夫
-     let fields: Vec<_> = record
-       .split(',')
-       .map(|field| field.trim())
-       .collect();
-     if cfg!(debug_assertions) {
-         // 输出到标准错误输出
-       eprintln!("debug: {:?} -> {:?}",
-              record, fields);
-     }
- 
-     let name = fields[0];
-     // 1. 尝试把fields[1]的值转换为f32类型的浮点数，如果成功，则把f32值赋给length变量
-     // 2. if let是一个匹配表达式，用来从=右边的结果中，匹配出length的值:
-     // 当=右边的表达式执行成功，则会返回一个Ok(f32)的类型，若失败，则会返回一个Err(e)类型，if let的作用就是仅匹配Ok也就是成功的情
-     // 况,如果是错误，就直接忽略，同时if let还会做一次解构匹配，通过Ok(length)去匹配右边的Ok(f32)，最终把相应的f32值赋给length
-     // 3. 当然你也可以忽视成功的情况，用if let Err(e) = fields[1].parse::<f32>() {...}匹配出错误，然后打印出来，但是没啥卵用
-     if let Ok(length) = fields[1].parse::<f32>() {
-         // 输出到标准输出
-         println!("{}, {}cm", name, length);
-     }
+        // Vec是vector的缩写，是一个可伸缩的集合类型，可以认为是一个动态数组
+        // <_>表示Vec中的元素类型由编译器自行推断，在很多场景下，都会帮我们省却不少功夫
+        let fields: Vec<_> = record.split(',').map(|field| field.trim()).collect();
+        if cfg!(debug_assertions) {
+            // 输出到标准错误输出
+            eprintln!("debug: {:?} -> {:?}", record, fields);
+        }
+
+        let name = fields[0];
+        // 1. 尝试把fields[1]的值转换为f32类型的浮点数，如果成功，则把f32值赋给length变量
+        // 2. if let是一个匹配表达式，用来从=右边的结果中，匹配出length的值:
+        // 当=右边的表达式执行成功，则会返回一个Ok(f32)的类型，若失败，则会返回一个Err(e)类型，if let的作用就是仅匹配Ok也就是成功的情
+        // 况,如果是错误，就直接忽略，同时if let还会做一次解构匹配，通过Ok(length)去匹配右边的Ok(f32)，最终把相应的f32值赋给length
+        // 3. 当然你也可以忽视成功的情况，用if let Err(e) = fields[1].parse::<f32>() {...}匹配出错误，然后打印出来，但是没啥卵用
+        if let Ok(length) = fields[1].parse::<f32>() {
+            // 输出到标准输出
+            println!("{}, {}cm", name, length);
+        }
     }
 }
 
@@ -114,12 +109,36 @@ fn mutable() {
     a = 20;
     println!("a = {}", a);
 
+    // 变量遮蔽(shadowing)
     let b = String::from("hello");
     println!("b = {}", b);
-    let (a, mut b): (bool,bool) = (true, false);
+    let (a, mut b): (bool, bool) = (true, false);
     // a = true,不可变; b = false，可变
     println!("a = {:?}, b = {:?}", a, b);
 
     b = true;
     assert_eq!(a, b);
+
+    // Rust 常量的命名约定是全部字母都使用大写，并使用下划线分隔单词，另外对数字字面量可插入下划线以提高可读性
+    const MAX_POINTS: u32 = 100_000;
+    println!("MAX_POINTS = {}", MAX_POINTS);
+
+    let x = 5;
+    // 在函数的作用域内对之前的x进行遮蔽
+    // 在被遮蔽后，无法再访问到之前的同名变量
+    let x = x + 1;
+    {
+        // 在当前的花括号作用域内，对之前的x进行遮蔽
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+    // println!("x = {}", x);
+    println!("The value of x is: {}", x);
+
+    // 字符串类型
+    let spaces = "   ";
+    // usize数值类型
+    let spaces = spaces.len();
+    // 变量的生命周期
+    println!("spaces = {}", spaces);
 }
